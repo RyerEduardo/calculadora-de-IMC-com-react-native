d0994a697f54707de5378a6f6a3ac4f01526a6dd
Eduardo <eduardo.oliveira@novaandradina.org>
2019-03-27 15:07:32 -0300

    parte de calculo pronto

--- App.js
+++ App.js
@@ -12,30 +12,37 @@ import {
   Text, 
   TextInput,
   TouchableOpacity,
-  View} 
-  from 'react-native';
+  View,
+} from 'react-native';
 
 
 type Props = {};
 export default class App extends Component<Props> {
 
-  constructor)(props){
+  constructor(props){
     super(props)
-    this.states = {altura:0, peso:0, resultado:0, resultadoText:""}
+    this.state = {altura:0, peso:0, resultado:0, resultadoText:""}
     this.calcular = this.calcular.bind(this)
+  }
+
+  calcular(){
+    let imc = this.state.peso / (this.state.altura * this.state.altura)
+    let s = this.state
+    s.resultado = imc
+    this.setState(s)
   }
 
 
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.endradas}>
-          <TextInput palceholder="Altura ex: 1.73" keyboardType="numeric" style={styles.input}/>
-          <TextInput palceholder="Peso ex: 90" keyboardType="numeric" style={styles.input}/>
+        <View style={styles.entradas}>
+          <TextInput placeholder="Altura ex: 1.73" keyboardType="numeric" style={styles.input} onChangeText={(altura)=>{this.setState({altura})}}/>
+          <TextInput placeholder="Peso ex: 90" keyboardType="numeric" style={styles.input} onChangeText={(peso)=>{this.setState({peso})}}/>
        </View>
-         <TouchableOpacity style={style.button} onPress={()=>{}}><Text style={styles.buttonText}>Calcular</Text> </TouchableOpacity>
-         <Text style={styles.resultado}>30</Text>
-         <Text style={(styles.resultado, {fontSize:35})}>Calcular</Text>
+         <TouchableOpacity style={styles.button} onPress={this.calcular}><Text style={styles.buttonText}>Calcular</Text></TouchableOpacity>
+         <Text style={styles.resultado}>{this.state.resultado.toFixed(2)}</Text>
+         <Text style={[styles.resultado, {fontSize:35} ]}>{this.state.resultadoText}</Text>
       </View>
     );
   }
@@ -47,30 +54,30 @@ const styles = StyleSheet.create({
     backgroundColor: '#F5FCFF',
   },
   entradas:{
-    flexDirection: "row",
+    flexDirection: 'row',
   },
   input:{
-    heigth: 70,
+    height: 80,
     textAlign: "center",
     width: "50%",
-    fontSize: 40,
+    fontSize: 25,
     marginTop: 20,
   },
   button:{
-    backgroundColor: "#89ffa5"
+    backgroundColor: "#89ffa5",
   },
   buttonText:{
     alignSelf: "center",
     padding: 30,
     fontSize: 25,
     color: "#6dc4a4",
-    fontWeigth: "bold",
+    fontWeight: "bold",
   },
   resultado:{
     alignSelf: "center",
-    color: "ligthgray",
+    color: "lightgray",
     fontSize:65,
     padding: 15,
-  }
+  },
   
 });
