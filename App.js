b996e43e28e5c217decfaa10d48f56cb36b6fbb1
Eduardo <eduardo.oliveira@novaandradina.org>
2019-03-27 12:56:39 -0300

    iniciando as funções

--- App.js
+++ App.js
@@ -7,23 +7,35 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {
+  StyleSheet, 
+  Text, 
+  TextInput,
+  TouchableOpacity,
+  View} 
+  from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
 type Props = {};
 export default class App extends Component<Props> {
+
+  constructor)(props){
+    super(props)
+    this.states = {altura:0, peso:0, resultado:0, resultadoText:""}
+    this.calcular = this.calcular.bind(this)
+  }
+
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Hello</Text>
-        <Text style={styles.instructions}>Agora vai </Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <View style={styles.endradas}>
+          <TextInput palceholder="Altura ex: 1.73" keyboardType="numeric" style={styles.input}/>
+          <TextInput palceholder="Peso ex: 90" keyboardType="numeric" style={styles.input}/>
+       </View>
+         <TouchableOpacity style={style.button} onPress={()=>{}}><Text style={styles.buttonText}>Calcular</Text> </TouchableOpacity>
+         <Text style={styles.resultado}>30</Text>
+         <Text style={(styles.resultado, {fontSize:35})}>Calcular</Text>
       </View>
     );
   }
@@ -32,18 +44,33 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  entradas:{
+    flexDirection: "row",
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  input:{
+    heigth: 70,
+    textAlign: "center",
+    width: "50%",
+    fontSize: 40,
+    marginTop: 20,
   },
+  button:{
+    backgroundColor: "#89ffa5"
+  },
+  buttonText:{
+    alignSelf: "center",
+    padding: 30,
+    fontSize: 25,
+    color: "#6dc4a4",
+    fontWeigth: "bold",
+  },
+  resultado:{
+    alignSelf: "center",
+    color: "ligthgray",
+    fontSize:65,
+    padding: 15,
+  }
+  
 });
